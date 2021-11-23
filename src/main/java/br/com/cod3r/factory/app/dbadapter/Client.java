d34a8785e17f48ca99742fbe7d6c1941303560e1
matg0mes <hotmus8500@gmail.com>
2021-11-23 02:57:27 -0300

    fix(db): create factory db

--- src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,16 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.factory.PostgresFactory;
+
 public class Client {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+//        DB db = new OracleFactory().getDatabase();
+        DB db = new PostgresFactory().getDatabase();
 
-	}
+        db.query("SELECT * FROM users");
+        db.query("INSERT INTO....");
+    }
+
 }
