873c2f9e20e1ffde3db1d33ce7efa560fa120c62
matg0mes <hotmus8500@gmail.com>
2021-11-23 03:49:07 -0300

    fix(abs-apple): create factory certification

--- src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,25 @@
 package br.com.cod3r.abstractfactory.apple;
 
-public class Client {
-	
-	public static void main(String[] args) {
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneFactory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRulesFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
 
-	}
+public class Client {
+
+    public static void main(String[] args) {
+        CountryRulesAbstractFactory rules = new BrazilianRulesFactory();
+
+        IPhoneFactory genXFactory = new IPhoneXFactory(rules);
+        IPhone11Factory gen11Factory = new IPhone11Factory(rules);
+
+        System.out.println("Ordering iphone X factory");
+        IPhone iPhone = genXFactory.orderIPhone("standard");
+
+        System.out.println("Ordering iphone 11 factory highEnd");
+        IPhone iPhone2 = gen11Factory.orderIPhone("highEnd");
+
+    }
 }
--- src/main/java/br/com/cod3r/abstractfactory/apple/model/certificate/BrazilianCertificate.java
+++ src/main/java/br/com/cod3r/abstractfactory/apple/model/certificate/BrazilianCertificate.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.abstractfactory.apple.model.certificate;
 
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+
 public class BrazilianCertificate implements Certificate {
 
 	public String applyCertification() {
